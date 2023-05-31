9b2c6491159ca42d291b783cd84890a63c334d51
DIogo Alpino <diogoalpino@gmail.com>
2023-05-31 16:05:36 -0300

    feat: CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import styles from './page.module.css'
+import Link from 'next/link'
+import './styles.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className='main'>
       <h1>Home Page</h1>
+      <Link className='link' href={`/livros`}>Livros</Link>
+      <Link className='link' href={`/autores`}>Autores</Link>
     </main>
   )
 }
